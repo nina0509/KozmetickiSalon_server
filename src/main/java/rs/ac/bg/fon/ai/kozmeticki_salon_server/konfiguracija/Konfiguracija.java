163dc18505ff3b928da3d53dafc701c8cb287099
Nikolina Baros <nikolina.baros12@gmail.com>
2024-09-06 04:39:00 +0200

    Dokumentovana klasa Konfiguracija

--- src/main/java/rs/ac/bg/fon/ai/kozmeticki_salon_server/konfiguracija/Konfiguracija.java
+++ src/main/java/rs/ac/bg/fon/ai/kozmeticki_salon_server/konfiguracija/Konfiguracija.java
@@ -12,14 +12,38 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
+ * Klasa koja upravlja učitavanjem, čuvanjem i manipulacijom konfiguracionih
+ * postavki aplikacije koje se nalaze u konfiguracionom fajlu.
  *
- * @author ninic
+ * Ova klasa koristi Properties objekat za čuvanje konfiguracionih podataka i
+ * omogućava pristup i modifikaciju tih podataka. Takođe, omogućava čuvanje
+ * izmena u konfiguracionom fajlu.
+ *
+ * Koristi Singleton kako bi se obezbedilo da samo jedan primerak Konfiguracije
+ * postoji tokom celokupne aplikacije.
  */
 public class Konfiguracija {
 
+    /**
+     * Privatna statička promenljiva koja čuva jedinu instancu Konfiguracija
+     * klase.
+     */
     private static Konfiguracija instance;
+    /**
+     * Properties objekat koji sadrži parove kljuceva i vrednosti i u kome
+     * cuvamo konfiguraciona podesavanja.
+     */
     private Properties konfiguracija;
 
+    /**
+     * Privatni konstruktor koji učitava konfiguraciju iz konfiguracionog fajla.
+     * Ovaj konstruktor učitava konfiguraciju iz fajla smještenog na putanji
+     * C:\\Users\\ninic\\Documents\\NetBeansProjects\\SEMINARSKI_SERVER\\config\\config.properties
+     *
+     * Ako učitavanje ne uspe, ispisuje grešku na konzolu i zapisuje grešku u
+     * log.
+     *
+     */
     private Konfiguracija() {
 
         konfiguracija = new Properties();
@@ -32,6 +56,11 @@ public class Konfiguracija {
 
     }
 
+    /**
+     * Vraća jedinu instancu Konfiguracija klase. Ako ona ne postoji, kreira je.
+     *
+     * @return Jedina instanca Konfiguracija klase.
+     */
     public static Konfiguracija getInstance() {
         if (instance == null) {
             instance = new Konfiguracija();
@@ -39,14 +68,36 @@ public class Konfiguracija {
         return instance;
     }
 
+    /**
+     * Vraća vrednost za dati ključ.
+     *
+     * @param key Ključ za koji se traži vrednost.
+     * @return Vrednost za dati kljuc, ili "n/a" ako ključ ne postoji.
+     */
     public String getProperty(String key) {
         return konfiguracija.getProperty(key, "n/a");
     }
 
+    /**
+     * Postavlja vrednost za dati ključ.
+     *
+     * @param key Ključ za koji se postavlja vrednost.
+     * @param value Vrednost koja se postavlja za dati ključ.
+     */
     public void setProperty(String key, String value) {
         konfiguracija.setProperty(key, value);
     }
 
+    /**
+     * Čuva izmene u konfiguracionom fajlu.
+     *
+     * Ova metoda čuva trenutne izmene u konfiguracionom fajlu koji se nalazi na
+     * putanji
+     * C:\\Users\\ninic\\Documents\\NetBeansProjects\\SEMINARSKI_SERVER\\config\\config.properties.
+     * Ako dođe do greške tokom čuvanja, ispisuje grešku na konzolu i zapisuje
+     * grešku u log.
+     *
+     */
     public void sacuvajIzmene() {
         try {
             konfiguracija.store(new FileOutputStream("C:\\Users\\ninic\\Documents\\NetBeansProjects\\SEMINARSKI_SERVER\\config\\config.properties"), null);
